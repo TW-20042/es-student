3885e5be1cc195a85394d9c2e2c30453b3fd9b53
Максим Дибижев <Maximd1566@gmail.com>
2026-09-17 21:28:18 +0300

    Сделана п1.2.3

--- 123-led-button-debounce/main.c
+++ 123-led-button-debounce/main.c
@@ -30,7 +30,8 @@ int main()
 
    while (1)
    {
-       bool current = gpio_get(BUTTON_PIN);
+       bool current = get_button_debounce(BUTTON_PIN);
+
 
        if (previous == true && current == false)
        {
